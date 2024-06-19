c26ef835d5d3f4ed2281a6fe6eda1ab494a71cb5
DJaegerScript <adjiedjaka0902@gmail.com>
2024-06-19 19:08:51 +0700

    fix typo

--- src/prediction/constants/predict.constant.ts
+++ src/prediction/constants/predict.constant.ts
@@ -2,5 +2,5 @@ import { InjectionType } from '@prisma/client'
 
 export const INJECTION_TYPE = {
   'Multi-Point Injection': InjectionType.MULTIPOINT,
-  'Direct Inject': InjectionType.DIRECT,
+  'Direct Injection': InjectionType.DIRECT,
 }
